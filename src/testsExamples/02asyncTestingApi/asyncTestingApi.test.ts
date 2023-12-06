83908c3710fa7fca43ff612887c930f85f15dbbd
RafalFak <rafalfak8@gmail.com>
2023-12-06 11:28:22 +0100

    feat: expanded async testing

--- src/testsExamples/02asyncTestingApi/asyncTestingApi.test.ts
+++ src/testsExamples/02asyncTestingApi/asyncTestingApi.test.ts
@@ -8,3 +8,30 @@ describe('fetchBtcPlnRate', () => {
     expect(result).toHaveProperty('status');
   });
 });
+
+const fetchDataMock = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('Mock Data');
+    }, 1000);
+  });
+};
+
+const fetchDataMockWithError = () => {
+  return new Promise((_, reject) => {
+    setTimeout(() => {
+      reject(new Error('Mock Error'));
+    }, 1000);
+  });
+};
+
+describe('fetchDataMock', () => {
+  test('fetch resolve', () => {
+    return expect(fetchDataMock()).resolves.toBe('Mock Data');
+  });
+
+  // when your async function should be rejected in some circumstances you can test it
+  test('fetch reject', () => {
+    return expect(fetchDataMockWithError()).rejects.toThrow('Mock Error');
+  });
+});
